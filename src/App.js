e95ec5d0d3a4809aa290b3fa4b9e5989f6aa0328
Tulio Gabriel <tullio_cba@hotmail.com>
2020-03-30 09:29:32 -0300

    Configurando rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-// import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 // import GlobalStyle from './styles/global';
 
 function App() {
     return (
         <>
-            {/* <Routes /> */}
+            <BrowserRouter>
+                <Routes />
+            </BrowserRouter>
+
             {/* <GlobalStyle /> */}
         </>
     );
