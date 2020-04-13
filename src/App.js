be1cd4c84b56df234a8ad4ff4c2a8660f5c484db
Tulio Gabriel <tullio_cba@hotmail.com>
2020-04-13 11:13:47 -0300

    React-toastify

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './config/ReactotronConfig';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
@@ -11,6 +12,7 @@ function App() {
     return (
         <Provider store={store}>
             <BrowserRouter>
+                <ToastContainer autoClose={3000} />
                 <GlobalStyle />
                 <Header />
                 <Routes />
